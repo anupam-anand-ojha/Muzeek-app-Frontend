cfab652990523807618afa7fa96e869ba37da736
Anupam Anand <theanupamojha999@gmail.com>
2026-05-19 23:37:27 +0530

    fix nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content">
     {/* Navbar */}
-    <nav className="navbar w-full fixed top-0 z-50 px-6 backdrop-blur-xl bg-black/30 bg-white/5 border-b border-white/20 text-white shadow-lg " >
+    <nav className="navbar w-full fixed top-0 z-50 px-6  backdrop-blur-xl bg-black/50 bg-white/5 border-b border-white/20 text-white shadow-lg " >
       <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
         {/* Sidebar toggle icon */}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
