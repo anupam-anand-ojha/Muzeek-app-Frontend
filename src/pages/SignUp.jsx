c52502c7f3aabdda3d7bbad3ab72467964e59a74
Anupam Anand <theanupamojha999@gmail.com>
2026-05-20 10:05:32 +0530

    upadte signUP

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -37,7 +37,7 @@ function SignUp() {
 
     <div className="hero  min-h-screen" style={{
     backgroundImage:
-      "url(https://images.unsplash.com/photo-1679746556827-a4722297a613?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fG11c2ljfGVufDB8MHw0fHx8Mg%3D%3D)",
+      "url(https://plus.unsplash.com/premium_photo-1682125853703-896a05629709?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
   }}>
       <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
         <div className="card-body">
