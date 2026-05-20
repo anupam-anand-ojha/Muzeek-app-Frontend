c54cb43772fe1e52f59c639162ca2fce815bf3df
Anupam Anand <theanupamojha999@gmail.com>
2026-05-20 23:32:49 +0530

    fix: setting

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Setting() {
   return (
-    <div className="h-screen overflow-hidden bg-cover bg-center bg-no-repeat"  style={{ backgroundImage: "url(https://images.unsplash.com/photo-1729857008877-c61ab58b300c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)" }}>
+    <div className="h-screen overflow-hidden bg-cover bg-center bg-no-repeat"  style={{ backgroundImage: "" }}>
       <div className='px-20 my-20'>
       <h1 className="text-3xl font-bold mb-6 ">
         Settings
