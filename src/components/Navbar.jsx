d8843b42cac44ec5a8178b014bb4f676e214cda5
Anupam Anand <theanupamojha999@gmail.com>
2026-08-04 02:33:57 +0530

    fix: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbar() {
         {/* Sidebar toggle icon */}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
       </label>
-      <div className="px-3 font-bold text-xl "> MUZEEK ♪</div>
+      <div className=" text-center text-white text-xl font-black lg:px-3 lg:font-black lg:text-xl"> MUZEEK ♪</div>
       {/* Right side */}
-  <div className="navbar-end ml-auto flex gap-3 items-center">
+  <div className="hidden lg:flex navbar-end ml-auto gap-3 items-center">
 
     {/* Song Search */}
     <input 
@@ -63,6 +63,19 @@ function Navbar() {
   </svg>
 </label>
   </div>
+
+  {/* // mobile theme  */}
+<div className='navbar-end lg:hidden ml-auto'>
+  <label className="toggle text-base-content">
+  <input type="checkbox" value="night" className="theme-controller" />
+
+  <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+  <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+
+</label>
+</div>
+
     </nav>
     
   </div>
